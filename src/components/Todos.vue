4c41a129d7d0698c659e31f94841c0670e4f0926
Dang Chuong Pham <dangchuong1999@outlook.com>
2020-12-15 21:26:25 +0200

    Todos component.

---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <h3>{{todo.title}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props: [
+    "todos"
+  ]
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
