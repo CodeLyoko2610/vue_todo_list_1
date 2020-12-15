5d2fc1f342148299dbdd381dd1ffa34d065692d9
Dang Chuong Pham <dangchuong1999@outlook.com>
2020-12-15 21:09:45 +0200

    Basic cleanup.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">    
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo 1',
+          completed: false        
+        },
+        {
+          id: 2,
+          title: 'Todo 2',
+          completed: false        
+        },
+        {
+          id: 3,
+          title: 'Todo 3',
+          completed: false        
+        },
+        {
+          id: 4,
+          title: 'Todo 4',
+          completed: false        
+        },
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
