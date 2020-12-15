55ee9ab8588c14a8a75facdb79a8099bc6c3c821
Dang Chuong Pham <dangchuong1999@outlook.com>
2020-12-16 00:37:17 +0200

    Add add-todo functionality.

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,13 +1,37 @@
 <template>
-  <form>
-    <input type="text" name="title" placeholder="Add todo...">
+  <form @submit="addTodo">
+    <input type="text" v-model="title" name="title" placeholder="Add todo...">
     <input type="submit" value="Submit" class="btn">
   </form>  
 </template>
 
 <script>
+import {v4 as uuidv4} from 'uuid'
+
 export default {
-  name: 'AddTodo'
+  name: 'AddTodo',
+  data(){
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addTodo(e){
+      e.preventDefault()
+
+      const newTodo = {
+        id: uuidv4(),
+        title: this.title,
+        completed: false
+      }
+      
+      //Emit to parent
+      this.$emit('add-todo', newTodo)
+
+      //Clear field
+      this.title = ''
+    }
+  }
 }
 </script>
 
