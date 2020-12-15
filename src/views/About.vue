2c7c056ebdc07097d5bf9edb04e0a9559f539760
Dang Chuong Pham <dangchuong1999@outlook.com>
2020-12-16 01:34:22 +0200

    Add author text.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Author - Dang Chuong Pham</h2>
     <p>
       This is my implementation of the Vue Crash Course by Brad Traversy
-    </p>
+    </p>    
   </div>
 </template>
